4ab2d6f5b9e6de7115b1cfbf02f413cbedd1ddb7
Matt Schulman <matthewdschulman@gmail.com>
2015-10-06 10:41:39 -0400

    get fit working...now onto predict

--- logreg.py
+++ logreg.py
@@ -3,7 +3,7 @@
     AUTHOR Eric Eaton
 '''
 
-
+import numpy as np
 
 class LogisticRegression:
 
@@ -11,8 +11,12 @@ class LogisticRegression:
         '''
         Constructor
         '''
-
-    
+        self.alpha = alpha
+        self.regLambda = regLambda
+        self.epsilon = epsilon
+        self.maxNumIters = maxNumIters
+        self.theta = None
+        self.theta_old = None
 
     def computeCost(self, theta, X, y, regLambda):
         '''
@@ -24,8 +28,14 @@ class LogisticRegression:
         Returns:
             a scalar value of the cost  ** make certain you're not returning a 1 x 1 matrix! **
         '''
-
-    
+        # cost = (-y * np.log(self.sigmoid(theta.T * X)) - (1 - y)*np.log(1 - self.sigmoid(theta.T * X))) + (regLambda/2.0)*(theta * theta)
+        # return cost.item(0)
+        n, d = X.shape
+        # Cost equation implementation.
+        Store = self.sigmoid(X * theta)
+        theta_minus_0 = np.delete(theta,[0], axis=0)
+        J_cost = (-y.T * np.log(Store) - (1.0-y).T * np.log(1.0-Store) )/n + regLambda/(2.0*n) * (theta_minus_0.T * theta_minus_0)
+        return J_cost.item(0)
     
     def computeGradient(self, theta, X, y, regLambda):
         '''
@@ -37,8 +47,17 @@ class LogisticRegression:
         Returns:
             the gradient, an d-dimensional vector
         '''
-    
+        n, d = X.shape
+        gradient = (X.T * (self.sigmoid(X * theta) - y) + regLambda*theta) / n
+        # l,f = theta.shape
+        # print "n = %d" % (n)
+        # print "d = %d" % (d)
+        # print "l = %d" % (l)
+        # print "f = %d" % (f)
 
+        # don't regularize the theta_0 parameter
+        gradient[0] = sum(self.sigmoid(X * theta) - y) / n
+        return gradient
 
     def fit(self, X, y):
         '''
@@ -47,6 +66,36 @@ class LogisticRegression:
             X is a n-by-d numpy matrix
             y is an n-dimensional numpy vector
         '''
+
+        a,b = X.shape
+        # add the 1's features
+        X = np.c_[np.ones((a,1)), X]
+
+        # create a random starting theta
+        self.theta = np.mat(np.random.rand(b + 1,1))
+
+        theta_old = self.theta
+        theta_new = self.theta
+        
+        i = 0
+        while i < self.maxNumIters:
+            theta_new = theta_old - self.alpha * self.computeGradient(theta_new, X, y, self.regLambda)
+            if self.hasConverged(theta_new, theta_old):
+                self.theta = theta_new
+                return
+            else: 
+                theta_old = np.copy(theta_new)
+                i = i + 1
+                # cost = self.computeCost(theta_new, X, y, self.regLambda)
+                # print "cost: ", cost
+
+        self.theta = theta_new
+
+    def hasConverged(self, theta_new, theta_old):
+        if np.linalg.norm(theta_new - theta_old) < self.epsilon:
+            return True
+        else:
+            return False
 
 
     def predict(self, X):
@@ -57,4 +106,22 @@ class LogisticRegression:
         Returns:
             an n-dimensional numpy vector of the predictions
         '''
+        n = X.shape[0]
+        X = np.c_[np.ones((n,1)), X]
+        return np.array(self.sigmoid(X * self.theta))
 
+    def sigmoid(self, z):
+        '''
+        This method wasn't provided in the hw template...
+        Computes sigmoid for both vectors and matrices
+        '''
+
+        # test to verify this works for matrices AND vectors
+        # test_z_matrix = np.matrix('1 2; 3 4')
+        # test_z_vector = np.array([2,3,1,0])
+        # test1 = 1.0 / (1.0 + np.exp(-test_z_vector))
+        # test2 = 1.0 / (1.0 + np.exp(-test_z_matrix))
+        # print test1
+        # print test2
+
+        return 1.0 / (1.0 + np.exp(-z))
